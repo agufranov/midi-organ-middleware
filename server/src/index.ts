7600ba2467cc007426d48437976e8f12bd3d15d4
ag <al.gufranov@gmail.com>
2022-08-30 10:03:10 +0300

    Sequencer on rxjs

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,5 +4,6 @@ import Sequencer from './sequencer'
 const PORT = 5042
 
 const app = express()
-console.log(new Sequencer())
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`))
+
+Sequencer.test()
--- server/src/sequencer.ts
+++ server/src/sequencer.ts
@@ -1,5 +1,49 @@
-export default class Sequencer implements ISequencer {
-    test() {
-        console.log('test')
+import { Observable, Subject, map, filter, distinctUntilChanged } from 'rxjs'
+
+interface InEvent { button: number, on: boolean }
+interface OutEvent<TNote> { note: TNote, on: boolean }
+
+export default class Sequencer<TNote extends number> {
+    data: TNote[] = []
+    cursor = 0
+    output: Observable<OutEvent<TNote>>
+    mapping: {[key: number]: TNote | null} = {}
+
+    constructor(source: Observable<InEvent>) {
+        this.output = source.pipe(
+            filter(({ on, button }) => (!on !== !this.mapping[button])),
+            map(this.transform),
+        )
+    }
+
+    private pickNextNote(): TNote {
+        if (!this.data.length) throw new Error('No data')
+        return this.data[this.cursor++ % this.data.length]
+    }
+
+    private transform = ({ button, on }: InEvent): OutEvent<TNote> => {
+        const note = on ? this.pickNextNote() : this.mapping[button]!
+        this.mapping[button] = on ? note : null
+        return { note, on }
+    }
+
+    static test() {
+        const inEvents = new Subject<InEvent>()
+
+        const seq = new this<number>(inEvents)
+        seq.data = [1, 3, 7]
+
+        inEvents.pipe(distinctUntilChanged((a, b) => a.on === b.on && a.button === b.button)).subscribe(e => console.log('-->', e))
+        seq.output.subscribe(e => console.log('<--', e))
+
+        inEvents.next({ button: 6, on: true })
+        inEvents.next({ button: 6, on: true })
+        inEvents.next({ button: 6, on: true })
+        inEvents.next({ button: 4, on: true })
+        inEvents.next({ button: 5, on: false })
+        inEvents.next({ button: 4, on: false })
+        inEvents.next({ button: 6, on: false })
+        inEvents.next({ button: 6, on: true })
+        inEvents.next({ button: 6, on: false })
     }
 }
